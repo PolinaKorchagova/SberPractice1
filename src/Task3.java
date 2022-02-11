55e4eb3c849ff3c1749d44804e81a0f822173b2d
Корочагова Полина <kors4523@gmail.com>
2022-02-11 16:40:07 +0300

    Практика СберТех. 1 урок. Корчагова Полина. Не получилось выполнить 7 задание

---
+++ src/Task3.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+
+        int a = 7;
+        for (int i = 0; a < 99;){
+            System.out.println(a);
+            a += 7;
+        }
+    }
+}
---
+++ src/Task4.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+        int n = 1000;
+        double[] array = new double[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.random();
+        }
+
+        double max = array[0];
+        double min = array[0];
+        double avg = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(max < array[i])
+                max = array[i];
+            if(min > array[i])
+                min = array[i];
+            avg += array[i] / array.length;
+        }
+
+        System.out.println("Максимальное значение = " + max);
+        System.out.println("Минимальное значение = " + min);
+        System.out.println("Среднее значение = " + avg);
+    }
+}
