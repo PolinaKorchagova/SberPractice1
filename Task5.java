27458a13efd5874c496d3d969c5bd1a6c93fcaa5
PolinaKorchagova <99384271+PolinaKorchagova@users.noreply.github.com>
2022-02-11 15:35:44 +0300

    Add files via upload

---
+++ Task5.java
@@ -0,0 +1,23 @@
+package com.company;
+public class Task5 {
+    public static void main(String[]args) {
+        int a = 2, b = 100;
+        for(int i = a; i <= b; i++){
+            if(k(i)){
+                  System.out.print(i + " ");
+            }
+        }
+    }
+    public static boolean k(int x) {
+        if (x < 2) return false;
+        int y;
+        for (int i = 2; i <= x / 2; i++) {
+            y = x % i;
+            if (y == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
